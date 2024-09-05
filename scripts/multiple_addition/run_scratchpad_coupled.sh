85c1be0066098c106f9daddbeb79b90bb964c657
Hanseul Cho <jhs4015@kaist.ac.kr>
2024-09-05 16:20:50 +0900

    more scripts

--- scripts/multiple_addition/run_scratchpad_coupled.sh
+++ scripts/multiple_addition/run_scratchpad_coupled.sh
@@ -4,6 +4,8 @@ n_train=10
 n_test=20
 m_train=6
 m_test=12
+maxpos_d=25
+maxpos_o=15
 n_layers=6
 n_heads=8
 lr=0.0001
@@ -17,14 +19,14 @@ bs=500
 
 python run_parallel.py \
     --group_name MultipleAdditionScratchpad_di${n_train}_${n_test}_op${m_train}_${m_test} \
-    --exp_name NoPE_fullpad_revout_maxpos_di${maxpos_d}_op${maxpos_o}_${n_layers}layers_${n_heads}head_Data${n_data} \
+    --exp_name coupled_fullpad_revout_maxpos_di${maxpos_d}_op${maxpos_o}_${n_layers}layers_${n_heads}heads_Data${n_data} \
     --seeds 0 1 2 \
-    --seeds_data 0 1  \
-    --devices 3 \
+    --seeds_data 0 1 \
+    --devices 2 \
     --num_exp_per_device 1 \
     --overrides \
         project_name='PositionCoupling with Scratchpad' \
-        model.position_encoding_type=none \
+        model.position_encoding_type=abs_learned \
         model.num_layers=$n_layers \
         model.num_heads=$n_heads \
         model.normalization_layer=rmsnorm \
@@ -33,12 +35,17 @@ python run_parallel.py \
         model.d_model=$d_model \
         model.d_ff=$d_ff \
         model.d_kv=$d_kv \
+        model.n_positions=$((maxpos_d+1)) \
+        ++model.d_positions=2 \
+        ++model.share_pe=False \
         model.save=True \
-        task=multiple_addition_scratchpad\
+        task=multiple_addition_scratchpad_coupled \
         task.reverse_input=False \
         task.reverse_output=True \
         task.reverse_output_order=False \
         task.padding=True \
+        task.max_position_digits=$maxpos_d \
+        task.max_position_operands=$maxpos_o \
         task.train.min_n_digits=1 \
         task.train.max_n_digits=$n_train \
         task.train.min_n_operands=2 \
